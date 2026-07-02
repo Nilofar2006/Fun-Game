b833f412da75f60294ab3633701867ec6ab938f3
Nilofar2006 <nilofarjamadar10@gmail.com>
2026-07-02 14:18:18 +0530

    Add files via upload

---
+++ logicGam.js
@@ -0,0 +1,64 @@
+let userScore = 0;
+let compScore = 0;
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const playGame = (userChoice) => {
+  console.log("user choice = ", userChoice);
+  // Computer choice
+  const compChoice = genCompChoice();
+  console.log("Comp choice = ", compChoice);
+
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+const drawGame = () => {
+  console.log("game was draw.");
+  msg.innerText = "Game was Draw. Play again";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    console.log("you win");
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    console.log("you lose");
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lose! ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
